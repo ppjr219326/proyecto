f4c09a01a2befb5601bd11ee9e794ced7f61964a
jose c <jj21andres@gmail.com>
2019-12-19 17:00:27 -0500

    primer proyecto

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+  ob_start();
+  require_once('includes/load.php');
+  if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
+?>
+<?php include_once('layouts/header.php'); ?>
+<div class="login-page">
+    <div class="text-center">
+       <h1>PROYECTO DEL GRUPO 5</h1>
+       <p>Iniciar sesión </p>
+     </div>
+     <?php echo display_msg($msg); ?>
+      <form method="post" action="auth.php" class="clearfix">
+        <div class="form-group">
+              <label for="username" class="control-label">Cliente</label>
+              <input type="name" class="form-control" name="username" placeholder="Usuario">
+        </div>
+        <div class="form-group">
+            <label for="Password" class="control-label">Password</label>
+            <input type="password" name= "password" class="form-control" placeholder="Contraseña">
+        </div>
+        <div class="form-group">
+                <button type="submit" class="btn btn-info  pull-right">Ingresar</button>
+        </div>
+    </form>
+</div>
+<?php include_once('layouts/footer.php'); ?>
